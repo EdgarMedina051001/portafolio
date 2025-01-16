92708d2cf1fa044e15303ec9b75a387369df8a63
Edgar Medina <edgar.medina.4639@gmail.com>
2025-01-16 17:22:20 -0600

    second commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,4 +6,17 @@ export default {
 		extend: {},
 	},
 	plugins: [],
+
+	"animation": {
+		"text-gradient": "text-gradient 1.5s linear infinite"
+	},
+	"keyframes": {
+		"text-gradient": {
+			"to": {
+				"backgroundPosition": "200% center"
+			}
+		}
+	}
+
 }
+
